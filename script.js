3976690a84fb5851c6cebd18764c51aa33bfbf4f
Anthony <gioant1@hotmail.com>
2023-08-03 16:23:01 -0400

    added new file to refactor constructors into classes
    created file to hold both book class & library class and refactoring code

---
+++ script.js
@@ -0,0 +1,12 @@
+class book {
+    constructor(bookCover, title, author, pages, year, status) {
+        this.bookCover = bookCover;
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.year = year;
+        this.status = status;
+    }
+}
+
+
