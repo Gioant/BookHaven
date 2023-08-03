afc6446ca0f74c4dcd6062108651a45237e50f77
Anthony <gioant1@hotmail.com>
2023-08-03 16:24:50 -0400

    added library classes & comments for reminder

--- script.js
+++ script.js
@@ -7,6 +7,30 @@ class book {
         this.year = year;
         this.status = status;
     }
+    //add related methods to books here
 }
 
 
+class Library {
+    constructor() {
+        this.library = [];
+    }
+
+    addBook(book) {
+        this.library.push(book);
+    }
+
+    removeBook(index) {
+        this.library.splice(index, 1);
+    }
+
+    changeBookStatus(index) {
+        if (this.library[index].status === 'Read') {
+            this.library[index].status = 'Not Read';
+        } else {
+            this.library[index].status = 'Read';
+        }
+    }
+
+    //add related methods to library here
+}
