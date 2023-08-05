51c0c118c0973b0093320f201f22ed214753346c
Anthony <gioant1@hotmail.com>
2023-08-05 00:43:03 -0400

    renamed file for clarity

--- newLogic.js
+++ newLogic.js
@@ -45,73 +45,73 @@ window.onclick = function (event) {
 }
 
 
+//BOOK CLASS
+class Book {
+    constructor(bookCover, title, author, pages, year, status) {
+        this._bookCover = bookCover;
+        this._title = title;
+        this._author = author;
+        this._pages = pages;
+        this._year = year;
+        this._status = status;
+    }
 
-// class Book {
-//     constructor(bookCover, title, author, pages, year, status) {
-//         this._bookCover = bookCover;
-//         this._title = title;
-//         this._author = author;
-//         this._pages = pages;
-//         this._year = year;
-//         this._status = status;
-//     }
+    // Getter and Setter for bookCover
+    get bookCover() {
+        return this._bookCover;
+    }
 
-//     // Getter and Setter for bookCover
-//     get bookCover() {
-//         return this._bookCover;
-//     }
+    set bookCover(newBookCover) {
+        this._bookCover = newBookCover;
+    }
 
-//     set bookCover(newBookCover) {
-//         this._bookCover = newBookCover;
-//     }
+    // Getter and Setter for title
+    get title() {
+        return this._title;
+    }
 
-//     // Getter and Setter for title
-//     get title() {
-//         return this._title;
-//     }
+    set title(newTitle) {
+        this._title = newTitle;
+    }
 
-//     set title(newTitle) {
-//         this._title = newTitle;
-//     }
+    // Getter and Setter for author
+    get author() {
+        return this._author;
+    }
 
-//     // Getter and Setter for author
-//     get author() {
-//         return this._author;
-//     }
+    set author(newAuthor) {
+        this._author = newAuthor;
+    }
 
-//     set author(newAuthor) {
-//         this._author = newAuthor;
-//     }
+    // Getter and Setter for pages
+    get pages() {
+        return this._pages;
+    }
 
-//     // Getter and Setter for pages
-//     get pages() {
-//         return this._pages;
-//     }
+    set pages(newPages) {
+        this._pages = newPages;
+    }
 
-//     set pages(newPages) {
-//         this._pages = newPages;
-//     }
+    // Getter and Setter for year
+    get year() {
+        return this._year;
+    }
 
-//     // Getter and Setter for year
-//     get year() {
-//         return this._year;
-//     }
+    set year(newYear) {
+        this._year = newYear;
+    }
 
-//     set year(newYear) {
-//         this._year = newYear;
-//     }
+    // Getter and Setter for status
+    get status() {
+        return this._status;
+    }
 
-//     // Getter and Setter for status
-//     get status() {
-//         return this._status;
-//     }
+    set status(newStatus) {
+        this._status = newStatus;
+    }
+}
 
-//     set status(newStatus) {
-//         this._status = newStatus;
-//     }
-// }
-
-
+//LIBRARY CLASS
 class Library {
     constructor() {
         this.library = [];
